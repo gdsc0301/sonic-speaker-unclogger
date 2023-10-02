0a8822078c48860eeafac356dff6497c5b65413b
Guilherme Carvalho <gdsc0301@gmail.com>
2023-10-02 14:33:10 -0300

    Initial Commit

---
+++ Sonic Speaker Unclogger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sonic Speaker Unclogger
+//
+//  Created by Guilherme Carvalho on 02/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sonic Speaker Unclogger/Sonic_Speaker_UncloggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sonic_Speaker_UncloggerApp.swift
+//  Sonic Speaker Unclogger
+//
+//  Created by Guilherme Carvalho on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Sonic_Speaker_UncloggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
